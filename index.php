110eba5ddc85c363ed88c98d960ac1829166767b
Samuel Oluwadare <oluwadaresamuel39@gmail.com>
2022-11-04 21:13:14 +0100

    forth_commit

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: *");
-?>
 
-<?php require_once('./db.php'); ?>
 
-<?php
+require_once('./db.php');
+
+
 
 $firstname = trim($_POST['firstname']);
 $lastname = trim($_POST['lastname']);
@@ -20,23 +20,27 @@ $hash_password = password_hash($password, PASSWORD_BCRYPT);
 // die($hash);
 
 $duplicate = mysqli_query($conn, "SELECT * FROM users WHERE email = '$email' OR password = '$hash_password'");
-// if(mysqli_num_rows($duplicate) > 0)
-// {
-//   echo '<script> alert("email or password is already in use"); </script>';
-// }
+
+// $result2 = mysqli_num_rows($duplicate);
+// die($result2);
+
 if(password_verify($confirm_password, $hash_password)){
     $sql = 'INSERT INTO users SET firstname = ?, lastname = ?, email = ?, password = ?';
     $stmt = mysqli_stmt_init($conn);
     if (!mysqli_stmt_prepare($stmt, $sql)) {
         die("Query failed");
         }
+    else if (mysqli_num_rows($duplicate) > 0)
+{
+ die("email or password is already in use");
+}
     else{mysqli_stmt_bind_param($stmt, 'ssss', $firstname,$lastname, $email, $hash_password);
     mysqli_stmt_execute($stmt);
     }
-    echo "registration is successful";
+    die("registration is successful");
 }
+
 else{
     echo "password does not match";
 }
 
-?>
